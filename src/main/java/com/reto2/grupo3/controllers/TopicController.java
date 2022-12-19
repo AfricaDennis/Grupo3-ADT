1d170087285c6ceb0ec4c283854f5b71ac7f7615
RodyArmoa <59371352+RodyArmoa@users.noreply.github.com>
2022-12-19 21:18:00 +0100

    Se agregar a TopicController el listado de topics

--- src/main/java/com/reto2/grupo3/controllers/TopicController.java
+++ src/main/java/com/reto2/grupo3/controllers/TopicController.java
@@ -1,5 +1,44 @@
 package com.reto2.grupo3.controllers;
 
+import com.reto2.grupo3.model.Favorite;
+import com.reto2.grupo3.model.FavoriteServiceModel;
+import com.reto2.grupo3.model.Topic;
+import com.reto2.grupo3.model.TopicServiceModel;
+import com.reto2.grupo3.repository.TopicRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("api")
 public class TopicController {
+
+    @Autowired
+    private TopicRepository TopicRepository;
+
+    @GetMapping("/topics")
+    public ResponseEntity<List<TopicServiceModel>>getTopic(){
+        Iterable<Topic> Topic = TopicRepository.findAll();
+        List<TopicServiceModel> response = new ArrayList<TopicServiceModel>();
+
+        for(Topic topic : Topic){
+            response.add(
+                    new TopicServiceModel(
+                            topic.getId(),
+                        null,
+                            topic.getIdTeacher()
+
+                    )
+            );
+        }
+        return new ResponseEntity<List<TopicServiceModel>>(response, HttpStatus.OK);
+
+    }
 
 }
