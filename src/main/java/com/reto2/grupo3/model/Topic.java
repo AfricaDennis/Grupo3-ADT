a251266bf8293a9cb9e725d0c5dbb547dc2585b7
in2dm3-v <africadela.dennismu@elorrieta-errekamari.com>
2022-12-19 17:57:18 +0100

    Topic change

--- src/main/java/com/reto2/grupo3/model/Topic.java
+++ src/main/java/com/reto2/grupo3/model/Topic.java
@@ -1,26 +1,9 @@
 package com.reto2.grupo3.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-import jakarta.persistence.*;
-
-@Entity
-@Table(name="topics")
 public class Topic {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Integer id;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "id_teacher", foreignKey=@ForeignKey(name = "Fk_id_teacher"))
-    @JsonManagedReference
-    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
-    private Teacher teacher;
-
-  @Column(name = "name")
     String name;
-    @Column(name="id_teacher", insertable = false, updatable = false)
     Integer idTeacher;
 
     public Topic() {
