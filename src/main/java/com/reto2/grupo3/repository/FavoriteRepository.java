bcaa27a18a2e38fba72f6d604dccc94d681b5db5
RodyArmoa <59371352+RodyArmoa@users.noreply.github.com>
2022-12-19 19:55:30 +0100

    Se agrega mapeado a la clase topic

--- src/main/java/com/reto2/grupo3/repository/FavoriteRepository.java
+++ src/main/java/com/reto2/grupo3/repository/FavoriteRepository.java
@@ -1,7 +1,4 @@
 package com.reto2.grupo3.repository;
 
-import com.reto2.grupo3.model.Favorite;
-import org.springframework.data.repository.CrudRepository;
-
-public interface FavoriteRepository extends CrudRepository<Favorite, Integer> {
+public class FavoriteRepository {
 }
