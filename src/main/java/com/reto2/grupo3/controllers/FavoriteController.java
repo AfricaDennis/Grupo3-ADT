9e36d9744d91fc7cbe057551aaa8be779e43f0dd
in2dm3-v <africadela.dennismu@elorrieta-errekamari.com>
2022-12-19 20:09:14 +0100

    Add Service

--- src/main/java/com/reto2/grupo3/controllers/FavoriteController.java
+++ src/main/java/com/reto2/grupo3/controllers/FavoriteController.java
@@ -1,4 +1,10 @@
 package com.reto2.grupo3.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api")
 public class FavoriteController {
+
 }
