672174f81559adb106724b46f8d335be85d91cef
in2dm3-v <africadela.dennismu@elorrieta-errekamari.com>
2022-12-19 20:22:47 +0100

    Add FavoriteController getAll

--- src/main/java/com/reto2/grupo3/controllers/FavoriteController.java
+++ src/main/java/com/reto2/grupo3/controllers/FavoriteController.java
@@ -1,13 +1,16 @@
 package com.reto2.grupo3.controllers;
 
+import com.reto2.grupo3.model.Favorite;
 import com.reto2.grupo3.model.FavoriteServiceModel;
 import com.reto2.grupo3.repository.FavoriteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -18,6 +21,20 @@ public class FavoriteController {
 
     @GetMapping("/favorites")
     public ResponseEntity<List<FavoriteServiceModel>> getFavorites(){
-        return null;
+        Iterable<Favorite> favorites = favoriteRepository.findAll();
+        List<FavoriteServiceModel> response = new ArrayList<FavoriteServiceModel>();
+
+        for(Favorite favorite : favorites){
+            response.add(
+                    new FavoriteServiceModel(
+                         favorite.getId(),
+                         null,
+                            favorite.getId_teacher(),
+                            null,
+                            favorite.getId_student()
+                    )
+            );
+        }
+        return new ResponseEntity<List<FavoriteServiceModel>>(response, HttpStatus.OK);
     }
 }
