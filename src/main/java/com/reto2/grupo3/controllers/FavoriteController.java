73c67bca1bd36b08c5e2cafb856be40749f38213
in2dm3-v <africadela.dennismu@elorrieta-errekamari.com>
2022-12-19 21:14:00 +0100

    Add FavoriteController getById

--- src/main/java/com/reto2/grupo3/controllers/FavoriteController.java
+++ src/main/java/com/reto2/grupo3/controllers/FavoriteController.java
@@ -1,12 +1,13 @@
 package com.reto2.grupo3.controllers;
 
 import com.reto2.grupo3.model.FavoriteServiceModel;
+import com.reto2.grupo3.model.StudentServiceModel;
+import com.reto2.grupo3.model.TeacherServiceModel;
 import com.reto2.grupo3.repository.FavoriteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -19,5 +20,23 @@ public class FavoriteController {
     @GetMapping("/favorites")
     public ResponseEntity<List<FavoriteServiceModel>> getFavorites(){
         return null;
+    }
+
+    @GetMapping("/favorites/{id}")
+    public ResponseEntity<FavoriteServiceModel> getFavoritesById(@PathVariable("id") Integer id){
+        Favorite favorite = favoriteRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NO_CONTENT, "No existe favorito"));
+        StudentServiceModel student = null;
+        TeacherServiceModel teacher = null;
+
+        FavoriteServiceModel response = new FavoriteServiceModel(
+                favorite.getId(),
+                teacher,
+                favorite.getId_teacher(),
+                student,
+                favorite.getId_student()
+        );
+
+        return new ResponseEntity<FavoriteServiceModel>(response, HttpStatus.OK);
     }
 }
