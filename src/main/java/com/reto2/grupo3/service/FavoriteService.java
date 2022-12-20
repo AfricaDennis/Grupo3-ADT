fda5a77763665bc83eac3e6f1dac59577efcf0cd
in2dm3-v <africadela.dennismu@elorrieta-errekamari.com>
2022-12-20 21:08:11 +0100

    Modified FavoriteService, FavoriteController.java, FavoriteServiceImpl.java

--- src/main/java/com/reto2/grupo3/service/FavoriteService.java
+++ src/main/java/com/reto2/grupo3/service/FavoriteService.java
@@ -7,5 +7,7 @@ import java.util.List;
 public interface FavoriteService {
     List<FavoriteServiceModel> getAll();
     FavoriteServiceModel getFavorite(Integer id);
-
+    int createFavorite(FavoriteServiceModel favoriteServiceModel);
+    int updateFavorite(FavoriteServiceModel favoriteServiceModel, Integer id);
+    void deleteFavoriteById(Integer id);
 }
