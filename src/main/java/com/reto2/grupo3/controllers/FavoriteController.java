0b4ebad72716dbfe73006a159584b9cf602e4c6f
in2dm3-v <africadela.dennismu@elorrieta-errekamari.com>
2022-12-20 21:08:11 +0100

    Modified FavoriteService, FavoriteController.java, FavoriteServiceImpl.java

--- src/main/java/com/reto2/grupo3/controllers/FavoriteController.java
+++ src/main/java/com/reto2/grupo3/controllers/FavoriteController.java
@@ -7,6 +7,7 @@ import com.reto2.grupo3.model.TeacherServiceModel;
 import com.reto2.grupo3.repository.FavoriteRepository;
 import com.reto2.grupo3.service.FavoriteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -25,9 +26,18 @@ public class FavoriteController {
     public ResponseEntity<List<FavoriteServiceModel>> getFavorites(){
         return new ResponseEntity<>(favoriteService.getAll(), HttpStatus.OK);
     }
-
     @GetMapping("/favorites/{id}")
     public ResponseEntity<FavoriteServiceModel> getFavoritesById(@PathVariable("id") Integer id){
         return new ResponseEntity<FavoriteServiceModel>(favoriteService.getFavorite(id), HttpStatus.OK);
     }
+    @DeleteMapping("/favorites/{id}")
+    public ResponseEntity<?> deleteFavoritesById(@PathVariable("id") Integer id) {
+        try {
+            favoriteService.deleteFavoriteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResponseStatusException(HttpStatus.NO_CONTENT, "No existe el empleado");
+        }
+    }
+
 }
