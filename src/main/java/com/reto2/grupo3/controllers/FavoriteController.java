8eb2aa5d213a3f56d8dd61a747e79ab7ee1ead2b
in2dm3-v <africadela.dennismu@elorrieta-errekamari.com>
2022-12-20 17:36:15 +0100

    Modified Favorite

--- src/main/java/com/reto2/grupo3/controllers/FavoriteController.java
+++ src/main/java/com/reto2/grupo3/controllers/FavoriteController.java
@@ -1,14 +1,17 @@
 package com.reto2.grupo3.controllers;
 
+import com.reto2.grupo3.model.Favorite;
 import com.reto2.grupo3.model.FavoriteServiceModel;
 import com.reto2.grupo3.model.StudentServiceModel;
 import com.reto2.grupo3.model.TeacherServiceModel;
 import com.reto2.grupo3.repository.FavoriteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -19,7 +22,21 @@ public class FavoriteController {
 
     @GetMapping("/favorites")
     public ResponseEntity<List<FavoriteServiceModel>> getFavorites(){
-        return null;
+        Iterable<Favorite> favorites = favoriteRepository.findAll();
+        List<FavoriteServiceModel> response = new ArrayList<FavoriteServiceModel>();
+
+        for(Favorite favorite : favorites){
+            response.add(
+                    new FavoriteServiceModel(
+                         favorite.getId(),
+                         null,
+                            favorite.getId_teacher(),
+                            null,
+                            favorite.getId_student()
+                    )
+            );
+        }
+        return new ResponseEntity<List<FavoriteServiceModel>>(response, HttpStatus.OK);
     }
 
     @GetMapping("/favorites/{id}")
