a84d907c09033e9f21fc1e49cabc2503e402870a
in2dm3-v <africadela.dennismu@elorrieta-errekamari.com>
2022-12-20 17:35:14 +0100

    Add Student and Teacher

--- src/main/java/com/reto2/grupo3/model/Teacher.java
+++ src/main/java/com/reto2/grupo3/model/Teacher.java
@@ -1,58 +1,46 @@
 package com.reto2.grupo3.model;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
-
-import java.util.List;
 
 @Entity
 @Table(name="teachers")
 public class Teacher {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "user_id ", referencedColumnName = "id", foreignKey = @ForeignKey(name = "fk_teacher_id"))
-    private User user;
+    private Integer id;
     @Column(length = 50)
     private String location;
     @Column(length = 50)
     private String shift;
     @Column(length = 5000)
     private String photo;
-
-
-    @OneToMany(mappedBy = "teacher",
-            cascade = CascadeType.ALL,
-            orphanRemoval = true,
-            fetch = FetchType.LAZY)
-    @JsonBackReference
-    private List<Favorite> favorites;
-
-    @OneToMany(mappedBy = "teacher",
-            cascade = CascadeType.ALL,
-            orphanRemoval = true,
-            fetch = FetchType.LAZY)
-    @JsonBackReference
-    private List<Opinion> opinions;
+    @JoinColumn(name = "id", foreignKey=@ForeignKey(name = "fk_user_teacher"))
+    @OneToOne(fetch = FetchType.LAZY)
+    private User user;
 
     public Teacher() {
     }
-
-    public Teacher(User user, String location, String shift, String photo, List<Favorite> favorites, List<Opinion> opinions) {
-        this.user = user;
+    public Teacher(Integer id, String location, String shift, String photo, User user) {
+        this.id = id;
         this.location = location;
         this.shift = shift;
         this.photo = photo;
-        this.favorites = favorites;
-        this.opinions = opinions;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
         this.user = user;
+    }
+
+    public Teacher(String location, String shift, String photo, User user) {
+        this.location = location;
+        this.shift = shift;
+        this.photo = photo;
+        this.user = user;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public String getLocation() {
@@ -79,31 +67,22 @@ public class Teacher {
         this.photo = photo;
     }
 
-    public List<Favorite> getFavorites() {
-        return favorites;
+    public User getUser() {
+        return user;
     }
 
-    public void setFavorites(List<Favorite> favorites) {
-        this.favorites = favorites;
-    }
-
-    public List<Opinion> getOpinions() {
-        return opinions;
-    }
-
-    public void setOpinions(List<Opinion> opinions) {
-        this.opinions = opinions;
+    public void setUser(User user) {
+        this.user = user;
     }
 
     @Override
     public String toString() {
         return "Teacher{" +
-                "user=" + user +
+                "id=" + id +
                 ", location='" + location + '\'' +
                 ", shift='" + shift + '\'' +
                 ", photo='" + photo + '\'' +
-                ", favorites=" + favorites +
-                ", opinions=" + opinions +
+                ", user=" + user +
                 '}';
     }
 }
