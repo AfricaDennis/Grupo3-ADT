a3e2cdee3ce3ff3f085d91c8c9dccce151bf801b
in2dm3-v <africadela.dennismu@elorrieta-errekamari.com>
2022-12-20 19:32:30 +0100

    Modified Models

--- src/main/java/com/reto2/grupo3/model/Student.java
+++ src/main/java/com/reto2/grupo3/model/Student.java
@@ -1,36 +1,38 @@
 package com.reto2.grupo3.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
+
+import java.util.List;
 
 @Entity
 @Table(name="students")
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-
-
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "user_id", referencedColumnName = "id", foreignKey = @ForeignKey(name = "fk_student_id"))
     private User user;
+    @OneToMany(mappedBy = "student",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true,
+            fetch = FetchType.LAZY)
+    @JsonBackReference
+    private List<Favorite> favorites;
+    @OneToMany(mappedBy = "student",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true,
+            fetch = FetchType.LAZY)
+    @JsonBackReference
+    private List<Opinion> opinions;
 
     public Student() {
     }
-    public Student(Long id, User user) {
-        this.id = id;
-        this.user = user;
-    }
-    public Student(User user) {
-        this.user = user;
-    }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
+    public Student(User user, List<Favorite> favorites, List<Opinion> opinions) {
+        this.user = user;
+        this.favorites = favorites;
+        this.opinions = opinions;
     }
 
     public User getUser() {
@@ -41,11 +43,27 @@ public class Student {
         this.user = user;
     }
 
+    public List<Favorite> getFavorites() {
+        return favorites;
+    }
+
+    public void setFavorites(List<Favorite> favorites) {
+        this.favorites = favorites;
+    }
+
+    public List<Opinion> getOpinions() {
+        return opinions;
+    }
+
+    public void setOpinions(List<Opinion> opinions) {
+        this.opinions = opinions;
+    }
     @Override
     public String toString() {
         return "Student{" +
-                "id=" + id +
-                ", user=" + user +
+                "user=" + user +
+                ", favorites=" + favorites +
+                ", opinions=" + opinions +
                 '}';
     }
 }
