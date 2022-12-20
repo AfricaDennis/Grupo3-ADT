4d975f9d3c8a4762ec542e78189d1ec6bfcd8ae9
in2dm3-v <africadela.dennismu@elorrieta-errekamari.com>
2022-12-20 19:32:30 +0100

    Modified Models

--- src/main/java/com/reto2/grupo3/model/Opinion.java
+++ src/main/java/com/reto2/grupo3/model/Opinion.java
@@ -1,17 +1,16 @@
 package com.reto2.grupo3.model;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name="favorites")
-public class Favorite {
+@Table(name = "opinion")
+public class Opinion {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    Integer id;
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "id_teacher", referencedColumnName = "user_id", foreignKey=@ForeignKey(name = "fk_teacher_favorites"))
+    @JoinColumn(name = "id_teacher", referencedColumnName = "user_id", foreignKey=@ForeignKey(name = "fk_teacher_opinions"))
     @JsonManagedReference
     @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
     private Teacher teacher;
@@ -20,30 +19,42 @@ public class Favorite {
     private Integer id_teacher;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "id_students", referencedColumnName = "user_id", foreignKey=@ForeignKey(name = "fk_student_favorites"))
+    @JoinColumn(name = "id_students", referencedColumnName = "user_id", foreignKey=@ForeignKey(name = "fk_student_opinions"))
     @JsonManagedReference
     @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
     private Student student;
 
     @Column(name="id_students", insertable = false, updatable = false)
     private Integer id_students;
+    @Column(length = 50)
+    String assesment;
+    @Column(length = 50)
+    String date;
+    @Column(length = 50)
+    String opinion;
 
-    public Favorite() {
+    public Opinion() {
     }
 
-    public Favorite(Integer id, Teacher teacher, Integer id_teacher, Student student, Integer id_students) {
+    public Opinion(Integer id, Teacher teacher, Integer id_teacher, Student student, Integer id_students, String assesment, String date, String opinion) {
         this.id = id;
         this.teacher = teacher;
         this.id_teacher = id_teacher;
         this.student = student;
         this.id_students = id_students;
+        this.assesment = assesment;
+        this.date = date;
+        this.opinion = opinion;
     }
 
-    public Favorite(Teacher teacher, Integer id_teacher, Student student, Integer id_student) {
+    public Opinion(Teacher teacher, Integer id_teacher, Student student, Integer id_students, String assesment, String date, String opinion) {
         this.teacher = teacher;
         this.id_teacher = id_teacher;
         this.student = student;
-        this.id_students = id_student;
+        this.id_students = id_students;
+        this.assesment = assesment;
+        this.date = date;
+        this.opinion = opinion;
     }
 
     public Integer getId() {
@@ -78,22 +89,49 @@ public class Favorite {
         this.student = student;
     }
 
-    public Integer getId_student() {
+    public Integer getId_students() {
         return id_students;
     }
 
-    public void setId_student(Integer id_students) {
+    public void setId_students(Integer id_students) {
         this.id_students = id_students;
+    }
+
+    public String getAssesment() {
+        return assesment;
+    }
+
+    public void setAssesment(String assesment) {
+        this.assesment = assesment;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getOpinion() {
+        return opinion;
+    }
+
+    public void setOpinion(String opinion) {
+        this.opinion = opinion;
     }
 
     @Override
     public String toString() {
-        return "Favorite{" +
+        return "Opinion{" +
                 "id=" + id +
                 ", teacher=" + teacher +
                 ", id_teacher=" + id_teacher +
                 ", student=" + student +
-                ", id_student=" + id_students +
+                ", id_students=" + id_students +
+                ", assesment='" + assesment + '\'' +
+                ", date='" + date + '\'' +
+                ", opinion='" + opinion + '\'' +
                 '}';
     }
 }
--- src/main/java/com/reto2/grupo3/model/TeacherServiceModel.java
+++ src/main/java/com/reto2/grupo3/model/TeacherServiceModel.java
@@ -1,4 +1,84 @@
 package com.reto2.grupo3.model;
 
+import java.util.List;
+
 public class TeacherServiceModel {
+    private User user;
+    private String location;
+    private String shift;
+    private String photo;
+    private List<Favorite> favorites;
+    private List<Opinion> opinions;
+
+    public TeacherServiceModel() {
+    }
+
+    public TeacherServiceModel(User user, String location, String shift, String photo, List<Favorite> favorites, List<Opinion> opinions) {
+        this.user = user;
+        this.location = location;
+        this.shift = shift;
+        this.photo = photo;
+        this.favorites = favorites;
+        this.opinions = opinions;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getShift() {
+        return shift;
+    }
+
+    public void setShift(String shift) {
+        this.shift = shift;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public List<Favorite> getFavorites() {
+        return favorites;
+    }
+
+    public void setFavorites(List<Favorite> favorites) {
+        this.favorites = favorites;
+    }
+
+    public List<Opinion> getOpinions() {
+        return opinions;
+    }
+
+    public void setOpinions(List<Opinion> opinions) {
+        this.opinions = opinions;
+    }
+
+    @Override
+    public String toString() {
+        return "TeacherServiceModel{" +
+                "user=" + user +
+                ", location='" + location + '\'' +
+                ", shift='" + shift + '\'' +
+                ", photo='" + photo + '\'' +
+                ", favorites=" + favorites +
+                ", opinions=" + opinions +
+                '}';
+    }
 }
