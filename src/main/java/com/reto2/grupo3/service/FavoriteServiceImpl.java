f6f3e56ac05c667fc7682b5666bc7009fa2f883f
in2dm3-v <africadela.dennismu@elorrieta-errekamari.com>
2022-12-21 17:37:51 +0100

    Modified FavoriteServiceImpl.java

--- src/main/java/com/reto2/grupo3/service/FavoriteServiceImpl.java
+++ src/main/java/com/reto2/grupo3/service/FavoriteServiceImpl.java
@@ -5,11 +5,13 @@ import com.reto2.grupo3.repository.FavoriteRepository;
 import com.reto2.grupo3.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class FavoriteServiceImpl implements FavoriteService{
     @Autowired
     FavoriteRepository favoriteRepository;
