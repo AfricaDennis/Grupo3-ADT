ff8f08ae13891f961264bceff7c7cb3bfb3d68f3
in2dm3-v <africadela.dennismu@elorrieta-errekamari.com>
2022-12-21 16:33:31 +0100

    Modified FavoriteService, FavoriteController.java, FavoriteServiceImpl.java

---
+++ src/main/java/com/reto2/grupo3/model/FavoritePostRequest.java
@@ -0,0 +1,67 @@
+package com.reto2.grupo3.model;
+
+public class FavoritePostRequest {
+    private Integer id;
+    private TeacherServiceModel teacher;
+    private Integer id_teacher;
+    private StudentServiceModel student;
+    private Integer id_student;
+
+    public FavoritePostRequest() {
+    }
+
+    public FavoritePostRequest(Integer id, TeacherServiceModel teacher, Integer id_teacher, StudentServiceModel student, Integer id_student) {
+        this.id = id;
+        this.teacher = teacher;
+        this.id_teacher = id_teacher;
+        this.student = student;
+        this.id_student = id_student;
+    }
+
+    public FavoritePostRequest(TeacherServiceModel teacher, Integer id_teacher, StudentServiceModel student, Integer id_student) {
+        this.teacher = teacher;
+        this.id_teacher = id_teacher;
+        this.student = student;
+        this.id_student = id_student;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public TeacherServiceModel getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(TeacherServiceModel teacher) {
+        this.teacher = teacher;
+    }
+
+    public Integer getId_teacher() {
+        return id_teacher;
+    }
+
+    public void setId_teacher(Integer id_teacher) {
+        this.id_teacher = id_teacher;
+    }
+
+    public StudentServiceModel getStudent() {
+        return student;
+    }
+
+    public void setStudent(StudentServiceModel student) {
+        this.student = student;
+    }
+
+    public Integer getId_student() {
+        return id_student;
+    }
+
+    public void setId_student(Integer id_student) {
+        this.id_student = id_student;
+    }
+}
--- src/main/java/com/reto2/grupo3/service/FavoriteService.java
+++ src/main/java/com/reto2/grupo3/service/FavoriteService.java
@@ -1,5 +1,6 @@
 package com.reto2.grupo3.service;
 
+import com.reto2.grupo3.model.FavoritePostRequest;
 import com.reto2.grupo3.model.FavoriteServiceModel;
 
 import java.util.List;
@@ -7,5 +8,7 @@ import java.util.List;
 public interface FavoriteService {
     List<FavoriteServiceModel> getAll();
     FavoriteServiceModel getFavorite(Integer id);
-
+    void deleteById(Integer id);
+    FavoriteServiceModel create(FavoritePostRequest favoritePostRequest);
+    FavoriteServiceModel update(Integer id, FavoritePostRequest favoritePostRequest);
 }
