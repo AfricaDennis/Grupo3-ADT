d284c97b461c2ef89c8017ef096db589f27f2324
in2dm3-v <africadela.dennismu@elorrieta-errekamari.com>
2022-12-21 20:26:44 +0100

    Modified Favorites almost done

---
+++ src/main/java/com/reto2/grupo3/repository/StudentRepository.java
@@ -0,0 +1,7 @@
+package com.reto2.grupo3.repository;
+
+import com.reto2.grupo3.model.Student;
+import org.springframework.data.repository.CrudRepository;
+
+public interface StudentRepository extends CrudRepository<Student, Integer> {
+}
